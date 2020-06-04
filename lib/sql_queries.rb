57772a42d7344dde422b4326249b3307dbc9e261
michael thomas <michael.thomas2014@gmail.com>
2020-06-04 01:28:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT users.name, users.age, SUM(pledges.amount)
    FROM pledges
    LEFT OUTER JOIN users
-   ON pledges.user_id = users.id ORDER BY(name);"
+   ON pledges.user_id = users.id GROUP BY(name) ORDER BY(name);"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
